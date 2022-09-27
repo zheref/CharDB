d373e957dbb06e44e3ac176d29255236d6553a50
Sergio Daniel <me@zheref.io>
2022-09-27 15:25:35 -0500

    Add CharTCA

---
+++ CharTCA/CharTCA/CharTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharTCAApp.swift
+//  CharTCA
+//
+//  Created by Sergio Daniel on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CharTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CharTCA/CharTCA/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CharTCA
+//
+//  Created by Sergio Daniel on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
